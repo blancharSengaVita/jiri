176c3448403e95bf82e88ed6710448d2c52102e8
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-11-21 18:40:14 +0100

    refactor ressources codes

--- app/View/Components/GuestLayout.php
+++ app/View/Components/GuestLayout.php
@@ -2,15 +2,20 @@
 
 namespace App\View\Components;
 
+use Closure;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
-use Illuminate\View\View;
+
 
 class GuestLayout extends Component
 {
     /**
      * Get the view / contents that represents the component.
      */
-    public function render(): View
+    public function render(): View|\Illuminate\Foundation\Application|Factory|Htmlable|Closure|string|Application
     {
         return view('layouts.guest');
     }
